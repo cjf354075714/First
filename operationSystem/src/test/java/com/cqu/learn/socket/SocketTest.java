fd22a0b028322871e2fff09a4b1215fd4d6b51bc
cjf354075714 <cjf354075714@outlook.com>
2019-10-16 19:05:11 +0800

    update socket and serverSocket

---
+++ operationSystem/src/test/java/com/cqu/learn/socket/SocketTest.java
@@ -0,0 +1,36 @@
+package com.cqu.learn.socket;
+
+import org.junit.Test;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author Feng
+ * @date 2019/10/16
+ */
+public class SocketTest {
+
+    @Test
+    public void test() {
+        Socket socket = null;
+        BufferedOutputStream bufferedOutputStream = null;
+        try {
+            socket = new Socket("127.0.0.1", 4433);
+            bufferedOutputStream = new BufferedOutputStream(socket.getOutputStream());
+            bufferedOutputStream.write("你好，服务端".getBytes(StandardCharsets.UTF_8));
+            bufferedOutputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bufferedOutputStream.close();
+                socket.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
