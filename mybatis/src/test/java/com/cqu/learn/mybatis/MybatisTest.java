f66917db8b16ef2b93486f06612dd5df802c5b29
曹俊锋 <caojfc@yonyou.com>
2019-11-21 16:26:36 +0800

    add mybatis module

---
+++ mybatis/src/test/java/com/cqu/learn/mybatis/MybatisTest.java
@@ -0,0 +1,17 @@
+package com.cqu.learn.mybatis;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+/**
+ * @author Feng
+ * @date 2019/11/21
+ */
+public class MybatisTest {
+
+    @Test
+    public void test() {
+
+    }
+}
