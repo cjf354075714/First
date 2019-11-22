08e589fba9651401c8ba42af3b33d01c66587f9a
曹俊锋 <caojfc@yonyou.com>
2019-11-22 16:58:58 +0800

    update data structure

---
+++ dataStructure/src/main/java/com/cqu/learn/datastructure/Map.java
@@ -0,0 +1,9 @@
+package com.cqu.learn.datastructure;
+
+/**
+ * @author Feng
+ * @date 2019/11/22
+ */
+public interface Map<K, V> {
+
+}
--- dataStructure/src/main/java/com/cqu/learn/datastructure/stack/Stack.java
+++
@@ -1,8 +0,0 @@
-package com.cqu.learn.datastructure.stack;
-
-/**
- * @author Feng
- * @date 2019/10/24
- */
-public interface Stack {
-}
