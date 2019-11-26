402e9b7331db84487e2a4e9e185996c296882e96
cjf354075714 <cjf354075714@outlook.com>
2019-11-26 22:11:02 +0800

    add basic module

---
+++ base/src/test/java/com/cqu/learn/base/basic/BasicTest.java
@@ -0,0 +1,17 @@
+package com.cqu.learn.base.basic;
+
+import org.junit.Test;
+
+/**
+ * @auther Feng
+ * @date 2019/11/25
+ **/
+public class BasicTest {
+
+    @Test
+    public void test() {
+        double a = 100000000.1d;
+        int b = 6;
+        System.out.println(1 + 2 + "3");
+    }
+}
